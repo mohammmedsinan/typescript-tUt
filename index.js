4c806d65224983f8b7a7959fefe7def0941ba791
mohammmedsinan <75750652+mohammmedsinan@users.noreply.github.com>
2023-01-14 06:33:49 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+exports.user = void 0;
+exports.user = { name: "Red John", age: 40 };
+console.log('Red John');
+console.log(exports.user.name);
---
+++ index.ts
@@ -0,0 +1,5 @@
+export const user = {name:"Red John",age:40}
+
+console.log('Red John');
+console.log(user.name);
+
